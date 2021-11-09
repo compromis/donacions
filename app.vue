959425494c058b52d068978f515fcc61edbca9f8
Guillem <guillem@disedit.com>
2021-11-09 14:33:23 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { BNav } from '@compromis/blobby'
+</script>
+
+<template>
+  <div class="background">
+    <b-nav>
+      <template #logo-append>
+        <nuxt-link to="/">Donacions</nuxt-link>
+      </template>
+    </b-nav>
+
+    <div class="container">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@compromis/blobby/scss/variables';
+.background {
+    position: relative;
+    background-image: url(./assets/images/background.jpg);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    min-height: 100vh;
+    z-index: 0;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-image: linear-gradient(
+        120deg,
+        rgba($gradient-primary-start, .95),
+        rgba($gradient-primary-end, .8) 50%,
+        rgba($gradient-primary-end, .4)
+      );
+      z-index: -1;
+    }
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { reactive } from 'vue'
+import { BInputGroup, BInput } from '@compromis/blobby'
+
+const form = reactive({
+  first_name: '',
+  last_name: '',
+  email: '',
+  DNI: ''
+})
+</script>
+
+<template>
+  <main class="index">
+    <header class="hero">
+      <h1 class="text-7xl text-white">Col·labora</h1>
+    </header>
+    <section class="section">
+      {{ form }}
+      <input type="text" v-model="form.first_name" />
+      <b-input-group title="Dades personals">
+        <b-input
+          variant="float"
+          label="Nom"
+          name="first_name"
+          v-model="form.first_name"
+          span="2"
+        />
+        <b-input
+          variant="float"
+          label="Cognoms"
+          name="last_name"
+          v-model="form.last_name"
+          span="2"
+        />
+        <b-input
+          variant="float"
+          type="email" 
+          label="E-mail"
+          name="email"
+          v-model="form.email"
+          span="2"
+        />
+        <b-input
+          variant="float" 
+          label="DNI/NIE"
+          name="DNI"
+          v-model="form.DNI"
+          span="2"
+        />
+      </b-input-group>
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  @import '@compromis/blobby/scss/variables';
+
+  .hero {
+    margin: 4vh 0;
+
+    h1 {
+      font-weight: 500;
+    }
+
+    .text-7xl {
+      font-size: 7rem;
+    }
+  }
+
+  .section {
+    color: $white;
+    max-width: 950px;
+  }
+</style>
